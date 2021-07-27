cdf8132b3d508874f5db6e91865f451cea253539
Zhijie Xu <zhijie.xu@sjsu.edu>
2021-07-26 17:36:50 -0700

    sidemenu css small bug fixed

--- src/Components/ToTopBtn/ToTopBtn.js
+++ src/Components/ToTopBtn/ToTopBtn.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 import "./ToTopBtn.css";
 
-function ToTopBtn(){
-    const scrollToTop = () => {
-        window.scrollTo({top: 0, behavior: 'smooth'});
-    }
+function ToTopBtn() {
+	const scrollToTop = () => {
+		window.scrollTo({ top: 0, behavior: "smooth" });
+	};
 
-    return(
-        <div className="top" onClick={scrollToTop}>
-            &#8673;
-        </div>
-    )
+	return (
+		<div className="top" onClick={scrollToTop}>
+			&#8673;
+		</div>
+	);
 }
 
 export default ToTopBtn;
